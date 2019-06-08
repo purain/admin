87e37d43a5c609c1b27dd3973198483503e4197a
purain <1073357107@qq.com>
2019-06-08 16:57:32 +0800

    order

--- src/app.jsx
+++ src/app.jsx
@@ -7,7 +7,8 @@ import Layout 			from 'component/layout/index.jsx';
 import Home   			from 'page/home/index.jsx';
 import UserList 		from 'page/user/index.jsx';
 import ProductRouter 	from 'page/product/router.jsx';
-import Order 	from 'page/order/index.jsx';
+import OrderList 	 	from 'page/order/index.jsx';
+import OrderDetail 	 	from 'page/order/detail.jsx';
 import ErrorPage 		from 'page/error/index.jsx';
 
 class App extends React.Component{
@@ -21,10 +22,11 @@ class App extends React.Component{
 					<Route exact path="/" component={Home}/>
 					<Route path="/product" component={ProductRouter}/>
 					<Route path="/product-category" component={ProductRouter}/>
-					<Route path="/order/index" component={Order}/>
+					<Route path="/order/index" component={OrderList}/>
+					<Route path="/order/detail/:orderNumber" component={OrderDetail}/>
 					<Route path="/user/index" component={UserList}/>
-					<Redirect from="/order" to='/order/index'/>
-					<Redirect from="/user" to='/user/index'/>
+					<Redirect exact from="/order" to='/order/index'/>
+					<Redirect exact from="/user" to='/user/index'/>
 					<Route component={ErrorPage} />
 				</Switch>
 			</Layout>
