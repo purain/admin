fd9af4e11d85cd07d9a575f484ee31dbcda85ee1
purain <1073357107@qq.com>
2019-06-14 16:17:20 +0800

    add postcss

--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,7 @@ module.exports = {
         test: /\.scss$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
+          use: ['css-loader', 'sass-loader', 'postcss-loader']
         })
       },
       // 图片的配置
